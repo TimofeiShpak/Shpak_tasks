20df1e9d07eddd4019308a5db7dbd485ab22e179
TimofeiShpak <tima.shpak.v@gmail.com>
2021-02-16 17:00:50 +0400

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const fileswatch   = 'html,htm,txt,json,md,woff2'
+
+const { src, dest, parallel, series, watch } = require('gulp')
+const browserSync  = require('browser-sync').create()
+const bssi         = require('browsersync-ssi')
+const sass         = require('gulp-sass')
+const sassglob     = require('gulp-sass-glob')
+const cleancss     = require('gulp-clean-css')
+const autoprefixer = require('gulp-autoprefixer')
+const rename       = require('gulp-rename')
+const del          = require('del')
+
+function browsersync() {
+	browserSync.init({
+		server: {
+			baseDir: 'app/',
+			middleware: bssi({ baseDir: 'app/', ext: '.html' })
+		},
+		notify: false,
+	})
+}
+
+function styles() {
+	return src(['app/styles/sass/**/*.scss', '!app/styles/sass/**/_*.scss'])
+		.pipe(sass())
+		.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'] }))
+		.pipe(cleancss({ level: { 1: { specialComments: 0 } },}))
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(dest('app/styles'))
+		.pipe(browserSync.stream())
+}
+
+function buildcopy() {
+	return src([
+		'app/styles/style.min.css',
+		'app/index.html',
+		'app/assets/**/*',
+	], { base: 'app/' })
+	.pipe(dest('dist'))
+}
+
+function cleandist() {
+	return del('dist/**/*', { force: true })
+}
+
+function startwatch() {
+	watch('app/styles/sass/**/*', { usePolling: true }, styles)
+	watch(`app/**/*.{${fileswatch}}`, { usePolling: true }).on('change', browserSync.reload)
+}
+
+exports.build   = series(cleandist, styles, buildcopy)
+exports.default = series(styles, parallel(browsersync, startwatch))
