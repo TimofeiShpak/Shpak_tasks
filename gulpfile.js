310d7f024d079da4122b0e20aa59a5abf510c65d
TimofeiShpak <tima.shpak.v@gmail.com>
2021-02-19 19:20:22 +0400

    change gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,36 +2,34 @@ const fileswatch   = 'html,htm,txt,json,md,woff2';
 
 const { src, dest, parallel, series, watch } = require('gulp');
 const browserSync  = require('browser-sync').create();
-const bssi         = require('browsersync-ssi');
 const sass         = require('gulp-sass');
 const cleancss     = require('gulp-clean-css');
-const autoprefixer = require('autoprefixer');
+const autoprefixer = require('gulp-autoprefixer');
 const rename       = require('gulp-rename');
-const postcss    = require('gulp-postcss')
 
 function browsersync() {
 	browserSync.init({
 		server: {
-			baseDir: 'app/',
-			middleware: bssi({ baseDir: 'app/', ext: '.html' })
+			baseDir: 'src/',
 		},
+		port: 3000,
 		notify: false,
 	})
 }
 
 function styles() {
-	return src(['app/styles/sass/**/*.scss', '!app/styles/sass/**/_*.scss'])
+	return src(['src/styles/sass/**/*.scss', '!src/styles/sass/**/_*.scss'])
 		.pipe(sass())
-		.pipe(postcss([ autoprefixer({ overrideBrowserslist: ['last 10 versions'] }) ]))
+		.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'] }))
 		.pipe(cleancss({ level: { 1: { specialComments: 0 } },}))
 		.pipe(rename({ suffix: ".min" }))
-		.pipe(dest('app/styles'))
+		.pipe(dest('src/styles'))
 		.pipe(browserSync.stream())
 }
 
 function startwatch() {
-	watch('app/styles/sass/**/*', { usePolling: true }, styles)
-	watch(`app/**/*.{${fileswatch}}`, { usePolling: true }).on('change', browserSync.reload)
+	watch('src/styles/sass/**/*', { usePolling: true }, styles)
+	watch(`src/**/*.{${fileswatch}}`, { usePolling: true }).on('change', browserSync.reload)
 }
 
 exports.default = series(styles, parallel(browsersync, startwatch))
