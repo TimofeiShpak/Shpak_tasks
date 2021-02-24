cb93b513d8e53b6a76ffe2170ecf8db0577ee781
TimofeiShpak <tima.shpak.v@gmail.com>
2021-02-24 20:45:13 +0400

    change media mixin, and margin mixin

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-const fileswatch   = 'html,htm,txt,json,md,woff2';
+const watchFiles = 'html,htm,txt,json,md,woff2';
 
 const { src, dest, parallel, series, watch } = require('gulp');
-const browserSync  = require('browser-sync').create();
-const sass         = require('gulp-sass');
-const cleancss     = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const cleancss = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
-const rename       = require('gulp-rename');
-const del          = require('del');
+const rename = require('gulp-rename');
+const del = require('del');
 
-function browsersync() {
+function browserSyncInit() {
 	browserSync.init({
 		server: {
-			baseDir: 'src/',
+			baseDir: 'dist/',
 		},
 		port: 3000,
 		notify: false,
@@ -19,32 +19,35 @@ function browsersync() {
 }
 
 function styles() {
-	return src(['src/styles/sass/**/*.scss', '!src/styles/sass/**/_*.scss'])
+	return src(['src/sass/**/*.scss', '!src/sass/**/_*.scss'])
 		.pipe(sass())
 		.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'] }))
-		.pipe(cleancss({ level: { 1: { specialComments: 0 } },}))
+		.pipe(cleancss({ level: { 1: { specialComments: 0 } }, }))
 		.pipe(rename({ suffix: ".min" }))
-		.pipe(dest('src/styles'))
+		.pipe(dest('dist'))
 		.pipe(browserSync.stream())
 }
 
-function startwatch() {
-	watch('src/styles/sass/**/*', { usePolling: true }, styles)
-	watch(`src/**/*.{${fileswatch}}`, { usePolling: true }).on('change', browserSync.reload)
-}
-
-function cleandist() {
+function cleanDist() {
 	return del('dist/**/*', { force: true })
 }
 
-function buildcopy() {
+function buildCopy() {
+	cleanDist();
+	styles();
 	return src([
-		'src/styles/style.min.css',
 		'src/index.html',
-		'src/assets/**/*.*'
+		'src/assets/**/*'
 	], { base: 'src/' })
-	.pipe(dest('dist'))
+		.pipe(dest('dist'))
 }
 
-exports.build   = series(cleandist, styles, buildcopy)
-exports.default = series(styles, parallel(browsersync, startwatch))
+function startWatch() {
+	watch('src/sass/**/*', { usePolling: true }, styles)
+	watch(['src/index.html','src/assets/**/*'], { usePolling: true }, buildCopy)
+	watch(`src/**/*.{${watchFiles}}`, { usePolling: true }).on('change', browserSync.reload)
+}
+
+exports.watch = startWatch;
+exports.build = series(cleanDist, styles, buildCopy);
+exports.default = series(cleanDist, buildCopy, styles, parallel(browserSyncInit, startWatch));
